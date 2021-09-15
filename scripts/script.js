49d5c0c23aaadddc30fbf8b6209570c0dafdbacf
sterreigm <90258029+sterreigm@users.noreply.github.com>
2021-09-15 16:42:09 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,5 @@
 // JavaScript Document
+var hamburgerButton = document.querySelector ('nav button:nth-of-type(2)')
+var menuList = document.querySelector ('nav ul li:nth-of-type(3) ul')
+
+GamepadButton.addEventlistener ('click')
